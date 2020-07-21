67bb1537d643e34be8fd0017476011279773a370
LevanceWam <lkwamley@fullsail.edu>
2020-07-21 19:01:54 -0400

    Learned how to publish a package to npm

---
+++ NodePackageManager/publishingPackages/lion-lib/index.js
@@ -0,0 +1,5 @@
+// we are going to export this simple function to the npm registry
+
+module.exports.add = function (a, b) {
+    return a + b
+};
---
+++ NodePackageManager/publishingPackages/test/index.js
@@ -0,0 +1,6 @@
+// here we are going to require the wamlion-lib module we installed
+
+const lion = require('wamlion-lib');
+
+let result = lion.add(1, 3);
+console.log(result);
