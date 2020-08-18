2c77a7df7b31ed399e822d1dbf699dff4445e958
LevanceWam <lkwamley@fullsail.edu>
2020-08-18 05:59:38 -0400

    learned how to query documents

---
+++ CRUDOperationUsingMongoose/QueryingDocuments/mongo-demo/index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to the Database'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Cactus Care',
+        author: 'Jack',
+        tags: ['watering', 'soil'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourse() {
+    const courses = await Course
+        .find({ // this return all of the courses 
+            author: 'vance'
+        })
+        .limit(10)
+        .sort({ // here we are sorting by acsending order. -1 will give us decsending
+            name: 1
+        })
+        .select({
+            name: 1,
+            tags: 1
+        });
+    console.log(courses);
+}
+
+getCourse();
