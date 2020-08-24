007802664655ab23ecd90ab06ae7dd6ec82acaaf
LevanceWam <lkwamley@fullsail.edu>
2020-08-24 13:22:03 -0400

    completed exercise2

--- CRUDOperationUsingMongoose/Exercise2/mongo-exercise/index.js
+++ CRUDOperationUsingMongoose/Exercise2/mongo-exercise/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongo-exercises')
-    .then(() => console.log('Connected to the Database'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
     name: String,
@@ -21,23 +19,28 @@ const Course = mongoose.model('course', courseSchema);
 async function getCourses() {
     return await Course
         .find({
-            tags: 'backend',
             isPublished: true
         })
+        .or([{
+                tags: 'backend'
+            },
+            {
+                tags: 'frontend'
+            }
+        ])
         .sort({
-            name: 1
+            price: -1
         })
         .select({
             name: 1,
-            author: 1
-        });
+            author: 1,
+            price: 1
+        })
 }
 
-async function displayCourses() {
-    // the reason this function is created is because it is not the job of the 
-    // getCourses function to display the courses its only function is to get the courses
+async function displayCourse() {
     const courses = await getCourses();
     console.log(courses);
 }
 
-displayCourses();
+displayCourse();
