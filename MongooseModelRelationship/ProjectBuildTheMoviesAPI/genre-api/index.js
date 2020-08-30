35123b44e71a0f8329b146bddbe220d3985eb9e4
LevanceWam <lkwamley@fullsail.edu>
2020-08-30 13:00:26 -0400

    ProjectBuildTheMoviesAPI

---
+++ MongooseModelRelationship/ProjectBuildTheMoviesAPI/genre-api/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const genre = require('./routes/genres');
+const home = require('./routes/home');
+const customer = require('./routes/customers');
+const movie = require('./routes/movies');
+
+mongoose.connect('mongodb://localhost/wamVid')
+    .then(() => console.log('connected to mongo'));
+
+app.use(express.json());
+app.use('/', home);
+app.use('/api/genre', genre);
+app.use('/api/customer', customer);
+app.use('/api/movie', movie);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}..`))
