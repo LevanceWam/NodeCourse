bdb60ff3ada18a7918c292b74851f47686d48995
LevanceWam <lkwamley@fullsail.edu>
2020-08-31 14:56:16 -0400

    learned about the objectId

---
+++ MongooseModelRelationship/ValidatingObjectIDs/genre-api/routes/customers.js
@@ -0,0 +1,73 @@
+const {
+    Customer,
+    validate
+} = require('../models/customer');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    const customers = await Customer
+        .find()
+        .sort({
+            name: 1
+        });
+
+    res.send(customers);
+});
+
+router.get('/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+
+    if (!customer) return res.status(404).send('Invalid ID');
+
+    res.send(customer);
+});
+
+router.post('/', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let customer = new Customer({
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold
+    });
+    customer = await customer.save();
+
+    res.send(customer);
+});
+
+router.put('/:id', async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    if (!customer) return res.status(404).send('Invalid ID');
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name: req.body.name
+    }, {
+        phone: req.body.phone
+    }, {
+        isGold: req.body.isGold
+    }, {
+        new: true
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id)
+
+    if (!customer) return res.status(404).send('Invalid ID');
+
+    res.send(customer);
+});
+
+
+module.exports = router;
