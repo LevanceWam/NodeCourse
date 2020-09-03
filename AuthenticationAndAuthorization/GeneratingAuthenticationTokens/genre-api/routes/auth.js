cab2caab4f8dcc0b869682e6fb23a8d521e2c16f
LevanceWam <lkwamley@fullsail.edu>
2020-09-03 09:26:17 -0400

    Generating Authentication Tokens completed

--- AuthenticationAndAuthorization/GeneratingAuthenticationTokens/genre-api/routes/auth.js
+++ AuthenticationAndAuthorization/GeneratingAuthenticationTokens/genre-api/routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const {
@@ -22,7 +23,11 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    res.send(true);
+    const token = jwt.sign({
+        _id: user._id
+    }, 'jwtPrivateKey');
+
+    res.send(token);
 });
 
 function validate(req) {
