618e1292ecdcace6f9811930e0709d5a7ec98112
LevanceWam <lkwamley@fullsail.edu>
2020-09-04 19:07:03 -0400

    learned about AuthorizationMiddleware

---
+++ AuthenticationAndAuthorization/AuthorizationMiddleware/genre-api/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send('Access denied. No token Provided');
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = decoded;
+        next();
+    } catch (ex) {
+        res.status(400).send('Invalid Token');
+    }
+}
+
+// module.exports = auth; 
+// Line 4 is shortcut to line 17
