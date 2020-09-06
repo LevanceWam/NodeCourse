067769f11479b802008b64ec3d44b7136cafab0a
LevanceWam <lkwamley@fullsail.edu>
2020-09-06 13:04:46 -0400

    Learned about RemovingTryCatchBlocks

---
+++ HandlingandLoggingErrors/RemovingTryCatchBlocks/genre-api/routes/genres.js
@@ -0,0 +1,73 @@
+const asyncMiddleware = require('../middleware/async');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
+const {
+    Genre,
+    validate
+} = require('../models/genre');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', asyncMiddleware(async (req, res, ) => {
+    const genres = await Genre
+        .find()
+        .sort({
+            name: 1
+        });
+
+    res.send(genres)
+
+}));
+
+router.get('/:id', asyncMiddleware(async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
+
+    if (!genre) return res.status(404).send('Invalid ID');
+
+    res.send(genre);
+}));
+
+router.post('/', auth, asyncMiddleware(async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = new Genre({
+        name: req.body.name
+    });
+    await genre.save();
+
+    res.send(genre);
+
+}));
+
+router.put('/:id', auth, asyncMiddleware(async (req, res) => {
+    const {
+        error
+    } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {
+        name: req.body.name
+    }, {
+        new: true
+    });
+
+    if (!genre) return res.status(404).send('Invalid ID');
+
+    res.send(genre);
+}));
+
+router.delete('/:id', [auth, admin], asyncMiddleware(async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id)
+
+    if (!genre) return res.status(404).send('Invalid ID');
+
+    res.send(genre);
+}));
+
+module.exports = router;
