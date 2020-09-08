9f442fa69931a68ea20ec14e02ebd2ad208e5d77
LevanceWam <lkwamley@fullsail.edu>
2020-09-07 23:27:42 -0400

    Learned how to ExtractTheValidateLogic

---
+++ HandlingandLoggingErrors/ExtractTheValidateLogic/genre-api/index.js
@@ -0,0 +1,12 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validate')();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => winston.info(`listening on port ${port}....`));
