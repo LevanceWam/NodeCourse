dd984fe754f1ec5b8b713d53e8f529f124834db6
LevanceWam <lkwamley@fullsail.edu>
2020-09-07 22:44:47 -0400

    Learned how to ExtractingTheDBLogic

---
+++ HandlingandLoggingErrors/ExtractingTheDBLogic/genre-api/index.js
@@ -0,0 +1,37 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const express = require('express');
+const app = express();
+
+require('./startup/routes')(app);
+require('./startup/db')();
+
+winston.handleExceptions(
+    new winston.transports.File({
+        filename: 'uncaughtException.log'
+    })
+);
+
+process.on('unhandledRejection', (ex) => {
+    throw ex;
+});
+
+winston.add(winston.transports.File, {
+    filename: 'logfile.log'
+});
+
+winston.add(winston.transports.MongoDB, {
+    db: 'mongodb://localhost/wamVid'
+});
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivatekey is not def')
+    process.exit(1);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}..`))
