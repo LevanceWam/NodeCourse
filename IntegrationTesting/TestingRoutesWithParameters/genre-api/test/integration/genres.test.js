e524f2dd53cc20376ee82f1acfc65c61eebbb541
LevanceWam <lkwamley@fullsail.edu>
2020-09-18 13:24:21 -0400

    Learned how to test routes with parameters

---
+++ IntegrationTesting/TestingRoutesWithParameters/genre-api/test/integration/genres.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest')
+const {
+    Genre
+} = require('../../models/genre');
+const mongoose = require('mongoose');
+
+let server;
+
+describe('/api/genres', () => {
+    beforeEach(() => {
+        server = require('../../index');
+    });
+    afterEach(async () => {
+        server.close()
+        await Genre.remove({});
+    });
+
+    describe('GET /', () => {
+        it('should return all genres', async () => {
+            await Genre.collection.insertMany([{
+                    name: 'genre1'
+                },
+                {
+                    name: 'genre2'
+                }
+            ]);
+
+            const res = await request(server).get('/api/genre');
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(genre => genre.name === 'genre2')).toBeTruthy();
+        });
+
+    });
+
+    describe('GET /:id', () => {
+        it('should return a genre if valid id is passed', async () => {
+            const genre = new Genre({
+                name: 'genre1'
+            });
+            await genre.save();
+
+            const res = await request(server).get('/api/genre/' + genre._id);
+
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', genre.name);
+        });
+    });
+});
