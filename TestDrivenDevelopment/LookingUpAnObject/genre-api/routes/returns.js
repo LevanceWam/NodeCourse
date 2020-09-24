f4b6d85818f92207be705f8f0332a40c73b23240
LevanceWam <lkwamley@fullsail.edu>
2020-09-23 22:01:02 -0400

    LookingUpAnObject

---
+++ TestDrivenDevelopment/LookingUpAnObject/genre-api/routes/returns.js
@@ -0,0 +1,20 @@
+const {
+    Rental
+} = require('../models/rental');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    if (!req.body.customerId) return res.status(400).send('customerId not provided');
+    if (!req.body.movieId) return res.status(400).send('movieId not provided');
+    res.status(401).send('Unauthorized');
+
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('Rental not Found');
+});
+
+module.exports = router;
