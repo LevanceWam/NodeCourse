a173747b0354e9a953e3416a4c66d6acb1ef9e71
LevanceWam <lkwamley@fullsail.edu>
2020-09-23 21:25:28 -0400

    refactored the test

---
+++ TestDrivenDevelopment/RefactoringTest/genre-api/test/integration/routes/return.test.js
@@ -0,0 +1,77 @@
+const {
+    Rental
+} = require('../../../models/rental');
+const {
+    User
+} = require('../../../models/users');
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+
+describe('api/returns', () => {
+    let server;
+    let customerId;
+    let movieId;
+    let rental;
+    let token;
+
+
+    beforeEach(async () => {
+        server = require('../../../index');
+
+        customerId = new mongoose.Types.ObjectId();
+        movieId = new mongoose.Types.ObjectId();
+        token = new User().generateAuthToken();
+
+        rental = new Rental({
+            customer: {
+                _id: customerId,
+                name: '12345',
+                phone: '12345'
+            },
+            movie: {
+                _id: movieId,
+                title: '12345',
+                dailyRentalRate: 2
+            }
+        });
+
+        await rental.save();
+    });
+
+    afterEach(async () => {
+        await Rental.remove({});
+        await server.close();
+    });
+
+
+    const exec = async () => {
+        return await request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({
+                customerId,
+                movieId
+            });
+    }
+
+    it('should return 401 if the client is not logged in', async () => {
+        token = '';
+        const res = await exec();
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if the customerId is not provided', async () => {
+        customerId = '';
+        const res = await exec();
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 400 if the movieId is not provided', async () => {
+        movieId = '';
+        const res = await exec();
+        expect(res.status).toBe(400);
+    });
+
+
+})
